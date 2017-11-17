ae23106e81daf86846822add7b94760215610181
Yelitza3125 <deyel_99@hotmail.com>
2017-11-17 13:55:12 -0500

    Añadiendo la función saveText

---
+++ js/app.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function(event){
+var textArea = document.getElementById('input');
+var textContainer = document.getElementById('container');
+var button = document.getElementById('tweet');
+var section = document.getElementsByTagName('section');
+
+//var parrafo = document.getElementById('content');
+
+button.addEventListener('click', function saveText(event){  
+  if(textArea.value) {
+    var parrafo = document.createElement('p');
+    parrafo.textContent = textArea.value;
+    textContainer.appendChild(parrafo);
+    textContainer.classList.add('container-text');
+    parrafo.classList.add('text');
+
+    textArea.value = '';
+  }
+
+  });
+});
+
+
+
+  
