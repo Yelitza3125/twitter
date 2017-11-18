55255580eefd05b9ba25f27eeb48b77400cf6006
Yelitza3125 <deyel_99@hotmail.com>
2017-11-17 22:57:37 -0500

    Creando la función contador

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var textArea = document.getElementById('input');
 var textContainer = document.getElementById('container');
 var button = document.getElementById('tweet');
 var section = document.getElementsByTagName('section');
+var formulario = document.getElementsByTagName('form');
+var length = textArea.getAttribute('maxlength');
+var contador = document.getElementById('count');
 
 //var parrafo = document.getElementById('content');
 
@@ -18,16 +21,30 @@ button.addEventListener('click', function saveText(event){
   }
   });
 
-  textArea.addEventListener('keypress', function actived(event) {
+  textArea.addEventListener('keypress', function enabled(event) {
     if(textArea.value) {
       button.disabled = false;
       button.classList.add('button-enabled');
+    } else if(textArea.value.length > 140) {
+      button.disabled = true;
     };
-
   });
 
+  textArea.addEventListener('keyup', function contadorText(event) {
+    contador.innerHTML = length;
+    contador.innerHTML = (length - textArea.value.length);
+  });
 });
 
 
-
+/*var el_t = document.getElementById('textarea');
+var length = el_t.getAttribute("maxlength");
+var el_c = document.getElementById('count');
+el_c.innerHTML = length;
+el_t.onkeyup = function () {
+  document.getElementById('count').innerHTML = (length - this.value.length);
+};
+<textarea id="textarea" name="text"
+ maxlength="500"></textarea>
+<span id="count"></span> */
   
