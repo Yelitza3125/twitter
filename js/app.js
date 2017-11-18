76ff4518ad0468257ced7cd59a58828629145512
Yelitza3125 <deyel_99@hotmail.com>
2017-11-18 02:05:54 -0500

    Dando estilo a la función time

--- js/app.js
+++ js/app.js
@@ -70,19 +70,18 @@ button.addEventListener('click', function saveText(event){
       horaContainer.appendChild(horaContent);
       //console.log(horaContent);
       container.appendChild(horaContainer);
+      horaContent.classList.add('time-text');
+      horaContainer.classList.add('time');
+  })
+
+  button.addEventListener('click', function separator(event) {
+    var line = document.createElement('hr');
+    //horaContainer.insertAfter(line);
+
+
   })
 
 });
 
 
-/*var el_t = document.getElementById('textarea');
-var length = el_t.getAttribute("maxlength");
-var el_c = document.getElementById('count');
-el_c.innerHTML = length;
-el_t.onkeyup = function () {
-  document.getElementById('count').innerHTML = (length - this.value.length);
-};
-<textarea id="textarea" name="text"
- maxlength="500"></textarea>
-<span id="count"></span> */
-  
+
