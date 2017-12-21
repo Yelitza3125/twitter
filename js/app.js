209f4745c8eb2509c0fb341d37bb771fb143c2a0
Yelitza3125 <deyel_99@hotmail.com>
2017-12-21 14:11:11 -0500

    jquery

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(event) {
+/*window.addEventListener('load', function(event) {
   var textArea = document.getElementById('input');
   var textContainer = document.getElementById('container');
   var button = document.getElementById('tweet');
@@ -62,4 +62,94 @@ window.addEventListener('load', function(event) {
     timeContent.classList.add('time-text');
     timeContainer.classList.add('time'); 
   });
+}); */
+
+jQuery(document).ready(function() { 
+  var btn = $('button');
+  var textArea = $('textarea');
+  var container = $('#container');
+  var maxLength = textArea.attr('maxlength');
+  var MAXCHARACTERS = 140;
+  
+ 
+  $(textArea).keypress(function(event) {
+    btn.attr('disabled', false);
+    btn.addClass('button-enabled');   
+    console.log(textArea.val());
+    $('#count').html(maxLength - textArea.val().length);
+
+    var number = maxLength - textArea.val().length;
+    console.log(number);
+    
+ 
+    $('#count').toggleClass('blue', number <= 20);
+    $('#count').toggleClass('red', number <= 10);    
+  });
+
+  $('button').click(function texto(event) {
+    var parrafo = $('<p/>', {
+      text: textArea.val(),
+      'class': 'text'
+    });
+
+    $('#container').prepend(parrafo);
+    $('#container').addClass('container-text');
+    event.preventDefault();
+
+    $('textarea').val('');
+    $('#count').html('');
+
+    $('button').attr('disabled', true);
+    $('button').removeClass('button-enabled');
+    $('button').addClass('disabled');
+ 
+  }); 
+    
+
+
+
+
+
+
+
+  /*   textArea.addEventListener('keyup', function accountant(event) {
+    count.innerHTML = maxLength;
+    count.innerHTML = (maxLength - textArea.value.length);
+    // console.log(length);
+  });
+
+
+  /*var capaDiasSemana = $('<div/>', { 'class': 'diassemana' });
+Cuando tienes muchos atributos, o cuando sus valores son dinámicos, el código resulta muy claro:
+
+var capaDiasSemana = $('<div/>', {
+    'class' : 'diassemana',
+    'id'    : 'id_' + user.id
+});
+  
+
+/*
+  $("<div>", {
+    'class': 'dropdown'
+}).append(
+    $('<ul>', {
+        'class': 'dropdown-menu',
+        'aria-labelledby': 'dropdownMenu1'
+    }).append(
+      $('<li>')
+        .append(
+          $('<a>',{
+            'href':'#',
+            'text':'Hola Mundo'
+          })
+        )
+    )
+).hide().appendTo('#otraDiv').fadeIn('slow'); */
+
+
+  /*
+  if (textArea.val().length === 0) {
+    btn.attr('disabled', false);
+  } */
+
 });
